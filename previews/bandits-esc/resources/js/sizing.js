c5b736aaeb5acfd850049ef193f921f41ff6b868
Jaykeb MR <44425964+JMR0102@users.noreply.github.com>
2019-12-05 18:03:28 +0800

    adjusted jQuery

---
+++ previews/bandits-esc/resources/js/sizing.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.header-dummy').height($('.header').outerHeight());
+    $('.landing').height(($(window).height() - $('.header').outerHeight()));
+    $('.landing-content .feature').height(($(window).height() - $('.header').outerHeight()));
+});
+
+$(window).resize(function() {
+    $('.header-dummy').height($('.header').outerHeight());
+    $('.landing').height(($(window).height() - $('.header').outerHeight()));
+    $('.landing-content .feature').height(($(window).height() - $('.header').outerHeight()));
+});
