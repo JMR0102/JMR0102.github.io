9cbada41550b148f72c617d548b9af1e179e9fd6
Jaykeb MR <44425964+JMR0102@users.noreply.github.com>
2019-12-05 18:09:37 +0800

    jQuery adjustments

--- previews/bandits-esc/resources/js/sizing.js
+++ previews/bandits-esc/resources/js/sizing.js
@@ -1,11 +1,17 @@
-$(function() {
-    $('.header-dummy').height($('.header').outerHeight());
-    $('.landing').height(($(window).height() - $('.header').outerHeight()));
-    $('.landing-content .feature').height(($(window).height() - $('.header').outerHeight()));
-});
+// $(function() {
+//     $('.header-dummy').height($('.header').outerHeight());
+//     $('.landing').height(($(window).height() - $('.header').outerHeight()));
+//     $('.landing-content .feature').height(($(window).height() - $('.header').outerHeight()));
+// });
 
-$(window).resize(function() {
-    $('.header-dummy').height($('.header').outerHeight());
-    $('.landing').height(($(window).height() - $('.header').outerHeight()));
-    $('.landing-content .feature').height(($(window).height() - $('.header').outerHeight()));
+// $(window).resize(function() {
+//     $('.header-dummy').height($('.header').outerHeight());
+//     $('.landing').height(($(window).height() - $('.header').outerHeight()));
+//     $('.landing-content .feature').height(($(window).height() - $('.header').outerHeight()));
+// });
+
+
+
+$(function() {
+    console.log('test');
 });
